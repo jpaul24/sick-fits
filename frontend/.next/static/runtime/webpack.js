38114cc480f52567ef628e1385249bb9c98a22b8
Joe Paul <joe.paul0712@gmail.com>
2020-01-27 19:14:02 +0000

    server side rendering with next ks

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2e0aec832093162898c0";
+/******/ 	var hotCurrentHash = "e27e644c5160fdc2e8ba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
