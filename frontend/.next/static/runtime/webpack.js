5b0c801a055ba980701670b30845731f3d7c87bc
Joe Paul <joe.paul0712@gmail.com>
2020-01-28 18:36:01 +0000

    frontend modules

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e27e644c5160fdc2e8ba";
+/******/ 	var hotCurrentHash = "e2494c65430880ac7643";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
