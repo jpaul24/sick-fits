613cc69d64044b5fa076f73ffe11b7edc09c41d2
Joe Paul <joe.paul0712@gmail.com>
2020-01-29 17:13:11 +0000

    adding db link

---
+++ backend/src/db.js
@@ -0,0 +1,13 @@
+// connects to the remote prisma DB and gives ability to
+// query with javascript
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+})
+
+module.exports = db
