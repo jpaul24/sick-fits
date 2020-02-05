9831f0fed2d94c0d34933bb516c8b4718efb4c1a
Joe Paul <joe.paul0712@gmail.com>
2020-02-05 20:02:24 +0000

    adding create item

---
+++ frontend/components/CreateItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/form';
+import formatMoney from '../lib/formatMoney';
+
+class CreateItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CreateItem;
