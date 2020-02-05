b65834ab0906d03265fae9ee824838cc1b095915
Joe Paul <joe.paul0712@gmail.com>
2020-02-05 20:05:30 +0000

    fixing the import for form

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import Form from './styles/form';
+import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 
 class CreateItem extends Component {
     render() {
         return (
-            <div>
+            <Form>
+                <fieldset>
+
+                </fieldset>
                 
-            </div>
+            </Form>
         );
     }
 }
